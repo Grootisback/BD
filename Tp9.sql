af5d4bd04c99386fa4da505d40078919e1686033
sebastien <seb.soveaux@hotmail.fr>
2018-12-04 14:52:37 +0100

    Update Tp9.sql

--- Tp9.sql
+++ Tp9.sql
@@ -7,16 +7,15 @@ FROM BLANCHE.Client C,
 WHERE c.cdtpcli =tc.cdtpcli
 	  AND UPPER(inttpcli)='ATELIER')
 	  AND C.cdcli=CTR.cdcli
-	  AND cdctr IS NULL)
+	  AND cdctr IS NULL);
 --b)
 SELECT rsnCli AS "Client"
 FROM BLANCHE.Client
 WHERE cdtpcli =(SELECT cdtpcli
 				FROM blanche.type_Client
 				WHERE UPPER(inttpcli)='ATELIER')
-	  AND cdcli IN (SELECT cdcli
-	  				FROM BLANCHE.contrat
-	  				WHERE cdctr IS NULL)
+	  AND cdcli NOT IN (SELECT cdcli
+	  				FROM BLANCHE.contrat;
 --c)
 SELECT rsnCli AS "Client"
 FROM BLANCHE.Client c
@@ -24,10 +23,9 @@ WHERE EXISTS (SELECT NULL
 				FROM blanche.type_Client tc
 				WHERE c.cdtpcli=tc.cdtpcli
 				AND UPPER(inttpcli)='ATELIER')
-	  AND EXISTS (SELECT null
+	  AND NOT EXISTS (SELECT null
 	  			  FROM BLANCHE.contrat ctr
-	  			  WHERE c.cdcli=ctr.cdcli
-	  			  AND cdctr IS NULL)
+	  			  WHERE c.cdcli=ctr.cdcli;
 
 --R29
 --a)
@@ -37,7 +35,7 @@ FROM BLANCHE.employé e,
 	 BLANCHE.employé DR
 WHERE e.dateNais>DR.dateNais
 	  AND upper(DR.nomEmp)='DAUTEL'
-	  AND upper(DR.prnmEmp)='RICHARD'
+	  AND upper(DR.prnmEmp)='RICHARD';
 --b)
 SELECT nomEmp||" "||prnmEmp AS "Employé",
 	   qualif AS "Qualification"
